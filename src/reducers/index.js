93ffa96c743af06c3119bcace1fe6ce965029b6b
gogopen85 <gogopen85@gmail.com>
2019-07-02 10:31:07 +0900

    Initial Commit

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import PostReducer from './reducer_posts'
+
+
+const rootReducer = combineReducers({
+  post: PostReducer
+});
+
+export default rootReducer;
